3b762d4dc0c6298da2d4ad6eaa888bf172e98898
jfmv.vt@gmail.com <jfmv.vt@gmail.com>
2017-05-04 18:34:41 +0200

    View Sectores View Buscar Instrumento View Instrumento Tiempo real en view Instrumento

--- src/main/java/com/txstockdata/iab_library/UIErrorHandler.java
+++ src/main/java/com/txstockdata/iab_library/UIErrorHandler.java
@@ -1,38 +1,37 @@
 package com.txstockdata.iab_library;
 
 import android.content.DialogInterface;
-import android.view.View;
 
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * © 2016 Jhon Fredy Magdalena Vila
  */
 public interface UIErrorHandler {
-    public abstract void showAlertError(String tittle, String message, DialogInterface.OnClickListener button2Listener);
+    void showAlertError(String tittle, String message, DialogInterface.OnClickListener button2Listener);
 
-    public abstract void showAlertError(String tittle, String message, String button1Text, DialogInterface.OnClickListener button1Listener, DialogInterface.OnClickListener button2Listener);
+    void showAlertError(String tittle, String message, String button1Text, DialogInterface.OnClickListener button1Listener, DialogInterface.OnClickListener button2Listener);
 
-    public abstract void logEventBuyIap();
+    void logEventBuyIap();
 
-    public abstract void logEventPurchaseSku(String sku);
+    void logEventPurchaseSku(String sku);
 
-    public abstract void handleError(JSONObject jsonObject);
+    void handleError(JSONObject jsonObject);
 
-    public abstract void showAlertCorrect(String title, String message);
+    void showAlertCorrect(String title, String message);
 
-    public abstract void handleError(StockDataException e);
+    void handleError(StockDataException e);
 
-    public abstract void handleError(Throwable e);
+    void handleError(Throwable e);
 
-    public abstract void alertException(String code, String message, Throwable cause, JSONObject jsonObject, DialogInterface.OnClickListener onClickListener);
+    void handleError(Throwable e, DialogInterface.OnClickListener onClickListener);
 
-    public abstract void logException(Throwable e);
+    void alertException(String code, String message, Throwable cause, JSONObject jsonObject, DialogInterface.OnClickListener onClickListener);
 
-    public abstract void handleUnknownError(Throwable e);
+    void logException(Throwable e);
+
+    void handleUnknownError(Throwable e);
+
 
 //    public abstract void alertException(String code, String message, Throwable cause, String option2Text, View.OnClickListener option2Listener);
 }
