8ae9a9aaa35c6339d91ea05b1bc9060473e89ff7
Jhon Magdalena <jfmv.vt@gmail.com>
2017-06-29 14:49:42 +0200

    Se muestra un alerta antes de cancelar orden

--- src/main/java/com/txstockdata/iab_library/UIErrorHandler.java
+++ src/main/java/com/txstockdata/iab_library/UIErrorHandler.java
@@ -1,6 +1,7 @@
 package com.txstockdata.iab_library;
 
 import android.content.DialogInterface;
+import android.support.annotation.StringRes;
 
 import org.json.JSONObject;
 
@@ -8,6 +9,10 @@ import org.json.JSONObject;
  * © 2017 Jhon Fredy Magdalena Vila
  */
 public interface UIErrorHandler {
+    void showAlertWarning(String title, String message, String positiveText, DialogInterface.OnClickListener positiveListener, String negativeText, DialogInterface.OnClickListener negativeListener, boolean cancelable);
+
+    void showAlertWarning(@StringRes int title, @StringRes int message, @StringRes int positiveText, DialogInterface.OnClickListener positiveListener, @StringRes int negativeText, DialogInterface.OnClickListener negativeListener, boolean cancelable);
+
     void showAlertError(String tittle, String message, DialogInterface.OnClickListener button2Listener);
 
     void showAlertError(String tittle, String message, String button1Text, DialogInterface.OnClickListener button1Listener, DialogInterface.OnClickListener button2Listener);
